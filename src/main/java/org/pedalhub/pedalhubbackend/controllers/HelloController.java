e0d7d7340a7a521801209ca57bdada69d9999ae4
teodorBurete <teodor.burete@yahoo.com>
2023-10-03 17:35:36 +0300

    successfully connected to azure mysql db <3

--- src/main/java/org/pedalhub/pedalhubbackend/controllers/HelloController.java
+++ src/main/java/org/pedalhub/pedalhubbackend/controllers/HelloController.java
@@ -1,5 +1,6 @@
 package org.pedalhub.pedalhubbackend.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String helloWorld() {
         return "Hello World!";
     }
