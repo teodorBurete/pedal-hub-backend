9cea7c7c4fdd47caed706a448977af745da67f9f
teodorBurete <teodor.burete@yahoo.com>
2023-10-18 18:31:21 +0300

    test on @CrossOrigin

---
+++ src/main/java/org/pedalhub/pedalhubbackend/config/WebConfig.java
@@ -0,0 +1,20 @@
+package org.pedalhub.pedalhubbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://localhost:3000")
+                .allowedMethods("GET", "POST")
+                .allowCredentials(true).maxAge(3600);
+    }
+}
