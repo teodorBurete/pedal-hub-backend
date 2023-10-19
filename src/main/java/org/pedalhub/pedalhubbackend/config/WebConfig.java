1d4c61cb36f047719b7421aa643d251a1b567733
teodorBurete <teodor.burete@yahoo.com>
2023-10-19 23:19:05 +0300

    CORS bug

--- src/main/java/org/pedalhub/pedalhubbackend/config/WebConfig.java
+++ src/main/java/org/pedalhub/pedalhubbackend/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*")
+        registry.addMapping("/**").allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS").allowedHeaders("*")
                 .allowCredentials(true).maxAge(3600);
     }
